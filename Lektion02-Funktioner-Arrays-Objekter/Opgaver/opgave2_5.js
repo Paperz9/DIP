f539ac2849a8ef1d2bc3bd317569b4046caa1d06
Paperz9 <kasperbergholdt@outlook.dk>
2023-02-02 18:07:52 +0100

    opgave 4 og 5

---
+++ Lektion02-Funktioner-Arrays-Objekter/Opgaver/opgave2_5.js
@@ -0,0 +1,6 @@
+let person = [];
+person[0] = {name:"Kasper", email:"Kasperbergholdt@outlook.dk", tlfNr:"30315057"};
+person[1] = {name:"Ellinor", email:"Fantasi@gmail.com", tlfNr:"12345678"};
+
+console.log(person[0]);
+console.log(person[1].name);
