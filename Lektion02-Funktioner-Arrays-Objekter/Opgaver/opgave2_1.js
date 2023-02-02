580972fe3b7dddd046d5465df23110204f36d0bc
Paperz9 <Kasperbergholdt@outlook.dk>
2023-02-02 10:05:54 +0100

    lektion02 - opgaver

---
+++ Lektion02-Funktioner-Arrays-Objekter/Opgaver/opgave2_1.js
@@ -0,0 +1,35 @@
+let list = [1,2,3,4,5,6,7];
+
+// Største tal i arrayet
+function max(array) {
+    let max = 0;
+    for (let i of array) {
+        if (i > max) {
+            max = i;
+        }
+    }
+    return max;
+}
+console.log(max(list));
+
+// Contains element i arrayet og retunerer true hvis det er der
+function contains(array, element) {
+    let found = false;
+    for (let e of array) {
+        if (e === element) {
+            found = true;
+        }
+    }
+    return found;
+}
+console.log(contains(list,6));
+
+// Finder summen af et array
+function sum(array) {
+    let sum = 0;
+    for (let i of array) {
+        sum += i;
+    }
+    return sum;
+}
+console.log(sum(list));
