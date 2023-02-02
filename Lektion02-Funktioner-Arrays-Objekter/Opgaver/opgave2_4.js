e98b2c970c09a39615b95b9b924964ce00d47eff
Paperz9 <Kasperbergholdt@outlook.dk>
2023-02-02 15:51:23 +0100

    update

---
+++ Lektion02-Funktioner-Arrays-Objekter/Opgaver/opgave2_4.js
@@ -0,0 +1,10 @@
+let word = "Der var engang en mand som hed Steen og han havde et ekstra ben";
+let words = word.split(" ");
+console.log(words[2]);
+
+function antalForskelligeOrd(array) {
+    let map = new Map();
+    let antal = 0;
+    let indeks = 0;
+
+}
