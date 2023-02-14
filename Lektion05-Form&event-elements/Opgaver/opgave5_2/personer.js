0bda09103db8b465ce4beaade186563f702baa6d
Paperz9 <Kasperbergholdt@outlook.dk>
2023-02-14 11:53:30 +0100

    Lektion05

---
+++ Lektion05-Form&event-elements/Opgaver/opgave5_2/personer.js
@@ -0,0 +1,7 @@
+let navnInput = document.getElementById("navn");
+let alderInput = document.getElementById("alder");
+let addButtom = document.getElementById("tilføj");
+let personerList = document.getElementById("personer");
+
+addButtom.addEventListener("click", () => {
+});
