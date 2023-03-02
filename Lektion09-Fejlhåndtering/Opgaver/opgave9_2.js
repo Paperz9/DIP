3a163eb454e1b9ea5540041f0fd04685e6fba7d5
Paperz9 <Kasperbergholdt@outlook.dk>
2023-03-02 11:57:36 +0100

    Lektion09 opgave9.1-2

---
+++ Lektion09-Fejlhåndtering/Opgaver/opgave9_2.js
@@ -0,0 +1,15 @@
+// Opgave 9.2
+
+function compare(a, b) {
+    if (a < b) {
+        return -1;
+    } else if (a > b) {
+        return 1;
+    } else {
+      return 0;  
+    }
+}
+
+function compareSort(array, fcn) {
+    
+}
